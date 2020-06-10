1d706a507ff06c39a181b239fec93dd88997238f
Ardiansyah <ardiansyah@ard333.com>
2020-06-10 21:24:33 +0700

    added 2 types of spring-graalvm-native (webmvc and webflux)

---
+++ spring-graalvm-native-webflux/src/main/java/com/ard333/springvsquarkus/springgraalvmnative/Resource.java
@@ -0,0 +1,29 @@
+package com.ard333.springvsquarkus.springgraalvmnative;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "resource", schema = "public")
+@AllArgsConstructor @NoArgsConstructor @Data
+public class Resource {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@Column(name = "resource_text")
+	private String resourceText;
+
+	@Column(name = "resource_string")
+	private String resourceString;
+
+}
---
+++ spring-graalvm-native-webflux/src/main/java/com/ard333/springvsquarkus/springgraalvmnative/ResourceController.java
@@ -0,0 +1,48 @@
+package com.ard333.springvsquarkus.springgraalvmnative;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/resource")
+public class ResourceController {
+
+	@Autowired
+	private ResourceService resourceService;
+
+	@GetMapping("/{id}")
+	public ResponseEntity<?> findById(@PathVariable("id") Long id) {
+		return ResponseEntity.ok(resourceService.findById(id));
+	}
+
+	@GetMapping("/by-page/{page}/{size}")
+	public ResponseEntity<?> findByPage(@PathVariable("page") Integer page, @PathVariable("size") Integer size) {
+		return ResponseEntity.ok(resourceService.findByPage(page, size));
+	}
+
+	@GetMapping("/by-resource-string/{resourceString}")
+	public ResponseEntity<?> findByResourceString(@PathVariable("resourceString") String resourceString) {
+		return ResponseEntity.ok(resourceService.findByResourceString(resourceString));
+	}
+
+	@PostMapping
+	public ResponseEntity<?> create(@RequestBody Resource resource) {
+		resourceService.create(resource);
+		return ResponseEntity.ok().build();
+	}
+
+	//@PutMapping("/{id}") //405 if using this on webflux
+	@RequestMapping(method = RequestMethod.PUT, value = "/{id}")
+	public ResponseEntity<?> update(@PathVariable("id") Long id, @RequestBody Resource resource) {
+		resourceService.update(id, resource);
+		return ResponseEntity.ok().build();
+	}
+
+}
